7508590c4b8360dfed23af3afa64218e112b6b05
Ephraim Oliphant <eeo003@bucknell.edu>
2021-12-07 17:07:13 -0500

    finished day1

---
+++ day1/day1.py
@@ -0,0 +1,10 @@
+import math as mt
+
+f=open("day1.txt","r")
+curCount=0
+prevVal=[mt.inf,mt.inf,mt.inf]
+for i,line in enumerate(f.readlines()):
+    if int(line)>prevVal[i%3]:
+        curCount+=1
+    prevVal[i%3]=int(line)
+print(curCount)
